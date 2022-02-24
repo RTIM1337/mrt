e2f84fdb34ddde2fbe7a487e309de8116197e9a5
RTIM1337 <99543418+RTIM1337@users.noreply.github.com>
2022-02-24 12:53:17 +0300

    Update atoruzonion.php
    fix

--- atoruzonion.php
+++ atoruzonion.php
@@ -25,6 +25,25 @@
         
           <p class="erorses" id="edr"></p>
           <button class="buttons">Вход</button>
+    
+          <?php
+require __DIR__ . '/auth.php';
+$login = getUserLogin();
+?>
+<html>
+<head>
+    <title>Главная страница</title>
+</head>
+<body>
+<?php if ($login === null): ?>
+<a href="/login.php">Авторизуйтесь</a>
+<?php else: ?>
+Добро пожаловать, <?= $login ?>
+<br>
+<a href="/logout.php">Выйти</a>
+<?php endif; ?>
+</body>
+</html>
       </form>
     </div>
 </body>
