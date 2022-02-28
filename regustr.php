17f26f6a7442cf16c0035d54d742004c69db7be0
RTIM1337 <99543418+RTIM1337@users.noreply.github.com>
2022-02-28 13:56:14 +0300

    fix

--- regustr.php
+++ regustr.php
@@ -15,6 +15,18 @@
       <?php
       include "php/header.php"
       ?>
+        <?
+If($UID) //если переменной нет, выводим форму
+{?>
+<form action="/" method="post">
+
+Логин: <input type="text" name="login" />
+Пароль: <input type="password" name="password" />
+
+<input type="submit" value="Войти" name="log_in" />
+</form>
+<?}
+?>
       <?
 If($UID) 
 ?>
